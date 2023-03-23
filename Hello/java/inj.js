e5300695ca045dd934cf9ffe6e8c4ee45678c9ae
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-03-23 11:25:20 +0700

    Add files via upload

---
+++ Hello/java/inj.js
@@ -0,0 +1,26 @@
+var showSecret = false;
+
+secretButton.addEventListener('click', toggleSecretState);
+updateSecretParagraph();
+
+function toggleSecretState() {
+    showSecret = !showSecret;
+    updateSecretParagraph();
+    updateSecretButton()
+}
+
+function updateSecretButton() {
+    if (showSecret) {
+        secretButton.textContent = 'Tekan dengan Jari Kelingking';
+    } else {
+        secretButton.textContent = 'Tekan dengan Jari Kelingking';
+    }
+}
+
+function updateSecretParagraph() {
+    if (showSecret) {
+        secretParagraph.style.display = 'block';
+    } else {
+        secretParagraph.style.display = 'none';
+    }
+}
---
+++ Hello/java/pop.js
@@ -0,0 +1,3 @@
+var secretButton = document.querySelector('#secret-button');
+var secretParagraph = document.querySelector('#secret-paragraph');
+var secretMarquee = document.querySelector('#secret-paragraph');
